070c4a456d5258011d3840f67f2fa3bb30e1ac41
Muhammad hamza <hamzashahzad020@gmail.com>
2025-09-05 23:51:26 +0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,5 +7,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
+  },
+  build: {
+    outDir: 'dist'
+  }
    
 })
